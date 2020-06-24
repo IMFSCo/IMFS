5ca4572078a39d6a2478cd9a539da514c73a9ace
Faizan <m.fayzan@icloud.com>
2020-06-25 01:36:31 +0300

    purchase and material requisition

---
+++ purchase_request/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Purchase Request",
+    "author": "SIT & think digital",
+    "version": "12.0.1",
+    "summary": "Use this module to have notification of requirements of "
+               "materials and/or external services and keep track of such "
+               "requirements",
+    "category": "Purchase Management",
+    "depends": ['mail',
+        "purchase",
+        "product","purchase_requisition"
+    ],
+    "data": [
+        "security/purchase_request.xml",
+        "security/ir.model.access.csv",
+        'data/ir_sequence.xml',
+        'wizard/wizard_view.xml',
+        "data/purchase_request_sequence.xml",
+        "data/purchase_request_data.xml",
+        "data/pr_mail_template.xml",
+        "views/purchase_request_view.xml",
+        'views/request_for_purchase_view.xml',
+        "reports/report_purchaserequests.xml",
+        "reports/report_modify_rfq.xml",
+        "views/purchase_request_report.xml",
+    ],
+    
+    'installable': True
+}
